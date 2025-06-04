bf845fdec32450ac434fb15bd32712e2dd73c8ad
Yash Shah <yashshah00@outlook.com>
2025-06-04 13:49:15 +0100

    Updated gen_test_metrics.py to compute metrics over different runs.

--- gen_test_metrics.py
+++ gen_test_metrics.py
@@ -1,3 +1,4 @@
+import glob
 import statistics
 
 import torch
@@ -6,29 +7,34 @@ from torch.utils.data import DataLoader
 from dataset import *
 from lightning_modules import FusedTrainingModule, TrainingModule
 
-data = GenomeDataset(Genome.CHM13, 18, read_overlap_len=None)
+data = GenomeDataset(Genome.CHM13, 21, read_overlap_len=None)
 loss_fn = torch.nn.CrossEntropyLoss()
 
 losses = []
 accuracies = []
 
-model = FusedTrainingModule.load_from_checkpoint(
-    "/home/yash/Projects/neural-genome-assembly/neural-genome-assembly/qrui6zq3/checkpoints/last.ckpt"
-).to("cuda")
-model.eval()
+transformer_runs = ["mm21ar3y", "enhty7hy", "j3wq9sew", "bakerhse", "l4bly10n"]
+mamba_runs = ["hxb2vrmr", "0qxwngia", "4aly6anh", "vnmmsuqu", "aai1emla"]
 
-for sample in data:
-    sample["reads"] = sample["reads"].to("cuda")
-    sample["target"] = sample["target"].to("cuda")
-    sample["overlap_lens"] = sample["overlap_lens"].to("cuda")
+for run in mamba_runs:
+    model = FusedTrainingModule.load_from_checkpoint(
+        glob.glob(
+            f"/home/yash/Projects/neural-genome-assembly/neural-genome-assembly/{run}/checkpoints/epoch*"
+        )[0]
+    ).to("cuda")
+    model.eval()
 
+    for sample in data:
+        sample["reads"] = sample["reads"].to("cuda")
+        sample["target"] = sample["target"].to("cuda")
+        sample["overlap_lens"] = sample["overlap_lens"].to("cuda")
 
-    with torch.no_grad():
-        result = model.forward(sample)
-    target = sample["target"][1:]
-    losses.append(loss_fn(result, target).item())
-    accuracy = (torch.argmax(result, dim=-1) == target).sum() / target.numel()
-    accuracies.append(accuracy.item())
+        with torch.no_grad():
+            result = model.forward(sample)
+        target = sample["target"][1:]
+        losses.append(loss_fn(result, target).item())
+        accuracy = (torch.argmax(result, dim=-1) == target).sum() / target.numel()
+        accuracies.append(accuracy.item())
 
 print(f"Loss = ({statistics.mean(losses)}, {statistics.stdev(losses)})")
 print(f"Accuracy = ({statistics.mean(accuracies)}, {statistics.stdev(accuracies)})")
